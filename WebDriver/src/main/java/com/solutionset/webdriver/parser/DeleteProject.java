53d3b3af0afc0615578f86ca7760a17a8a0b83fe
atrissel <atrissel@solutionset.com>
2013-06-17 16:45:05 -0700

    fixit runs for workflow

---
+++ WebDriver/src/main/java/com/solutionset/webdriver/parser/DeleteProject.java
@@ -0,0 +1,62 @@
+package com.solutionset.webdriver.parser;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class DeleteProject {
+
+	
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+
+		final String inputFile = "/Users/adamtrissel/git/JiraMigration/WebDriver/src/test/resources/testDeletePage.html";
+
+		
+		File input = new File(inputFile);
+		Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
+		
+		Elements projectTable = doc.select("table.aui");
+		
+		if (projectTable.size() >= 1) {
+			
+			Element project = projectTable.first();
+
+			Elements deleteNodes = project.select("a");
+			
+			for (Element node : deleteNodes){
+				
+				//System.out.println(node.html());
+				
+				if (node.toString().contains("delete_project_")){
+					
+					System.out.println(node.toString()	);
+					
+				}
+				
+			}
+			
+			//System.out.println(deleteNodes);
+			
+			
+			
+		}
+			
+			
+			
+		
+//		System.out.println(projectTable.html());
+//		System.out.println(projectTable.size());
+//		
+	}
+
+}
